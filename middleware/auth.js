3cb32c208c9d3246687c65f18b205d4110f09b62
Gutri Rahmad Zuwa <gutri363696@gmail.com>
2022-12-03 11:06:05 +0700

    modul_17

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,13 +4,15 @@ const jwt = require('jsonwebtoken');
 SECRET = process.env.SECRET
 const Auth = {
     verifyToken(req, res, next){
-        token=req.cookies["JWT"]
+        //token=req.cookies["JWT"]
+        const {token} = req.body
         console.log(token);
         if (token) {
             // 12. Lalukan jwt verify 
             const verified = jwt.verify(token,SECRET)
             if(verified){
               console.log("Verification Success")
+              req.verified = verified
               return next();
             } else{
               return res.status(401).send(error);
